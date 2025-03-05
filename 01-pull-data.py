e43f70701968c078d7b59a85603d5013f4b829cc
nicholas-tedesco <nicholas.r.tedesco@gmail.com>
2025-03-05 11:43:33 -0500

    gathered data links from lichess.org

--- 01-pull-data.py
+++ 01-pull-data.py
@@ -9,18 +9,34 @@
 
 import pandas as pd 
 import requests
-import beautifulsoup4 as bs4
+from bs4 import BeautifulSoup
+import re
 
 
-# lichess API -------------------------------------------------------------------------------------
+# retrieve HTML for lichess database webpage ------------------------------------------------------
 
-url = 'https://www.lichess.org/api/games'
+url = 'https://database.lichess.org/'
 
-params = {
-    'rated': 'true', 
-    'eloMin': 2000, 
-    'max': 10
-}
+r = requests.get(url) 
+page_html = r.text 
 
-r = requests.get(url, params=params) 
-print(r.text)
+
+# extract data download links, then filter --------------------------------------------------------
+
+soup = BeautifulSoup(page_html, features='html.parser')
+link_elements = soup.find_all('a')
+links = [element.get('href') for element in link_elements]
+
+standard_links = [link for link in links if 'standard/' in link and 'pgn.zst' in link and 'torrent' not in link]
+
+include_years = ('2024', '2025')
+final_links = [link for link in standard_links if any(year in link for year in include_years)]
+
+
+# for each dataset, only keep games above ELO threshold -------------------------------------------
+
+test_link = final_links[0]
+min_elo = 2200
+
+
+
