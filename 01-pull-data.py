392aed219581c57b41779f875be62458f0cdd217
nicholas-tedesco <nicholas.r.tedesco@gmail.com>
2025-03-05 22:47:19 -0500

    proof of concept streaming to data

--- 01-pull-data.py
+++ 01-pull-data.py
@@ -10,7 +10,8 @@
 import pandas as pd 
 import requests
 from bs4 import BeautifulSoup
-import re
+import zstandard as zstd 
+import io 
 
 
 # retrieve HTML for lichess database webpage ------------------------------------------------------
@@ -30,22 +31,70 @@ links = [element.get('href') for element in link_elements]
 standard_links = [link for link in links if 'standard/' in link and 'pgn.zst' in link and 'torrent' not in link]
 
 include_years = ('2024', '2025')
-final_links = [link for link in standard_links if any(year in link for year in include_years)]
+filtered_links = [link for link in standard_links if any(year in link for year in include_years)]
+
+final_links = [db_url + link for link in filtered_links]
 
 
 # for each dataset, only keep games above ELO threshold -------------------------------------------
 
-test_link = db_url + final_links[0]
-min_elo = 2200
+def parse_lichess_stream(url, min_elo):
 
-print(test_link)
+    with requests.get(url, stream=True) as response:
 
-# s = requests.Session() 
-# with s.get(test_link, stream=True) as resp: 
-#     for line in resp.iter_lines(): 
-#         if line: 
-#             print(line) 
-#             raise Exception('stop')
+        response.raise_for_status()
+        dctx = zstd.ZstdDecompressor()
+
+        with dctx.stream_reader(response.raw) as reader:
+
+            buffer = io.TextIOWrapper(reader, encoding='utf-8')
+
+            keep_white_elo = [] 
+            keep_black_elo = [] 
+            keep_pgn = [] 
+
+            game_details = {} 
+
+            keep = False
+            skip = True
+
+            for line in buffer:
+
+                line = line.strip() 
+
+                if line.startswith('['): 
+
+                    if 'WhiteElo' in line: 
+                        elo = int(line.split('"')[1])
+                        game_details['WhiteElo'] = elo 
+                        if elo >= min_elo: 
+                            keep = True 
+
+                    elif 'BlackElo' in line: 
+                        elo = int(line.split('"')[1])
+                        game_details['BlackElo'] = elo 
+                        if elo >= min_elo: 
+                            keep = True 
+
+                elif line: 
+                    if keep: 
+                        game_details['PGN'] = line 
+
+                else: 
+                    if skip:
+                        skip = False
+                        continue 
+                    if keep: 
+                        keep_white_elo.append(game_details['WhiteElo'])
+                        keep_black_elo.append(game_details['BlackElo'])
+                        keep_pgn.append(game_details['PGN'])
+
+                        print(game_details.items())
+
+                    keep = False 
+                    skip = True
+                    game_details = {} 
 
 
-
+for link in final_links: 
+    parse_lichess_stream(link, 2000) 
